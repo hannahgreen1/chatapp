97aef70abd9c483e0dbe4fa7237e2a8443284c44
Roosa <roosa.paivansalo@codeclan.com>
2021-11-30 15:51:33 +0000

    Send message to database functionality added

--- react-native-firebase/src/screens/ProfileScreen/ProfileScreen.js
+++ react-native-firebase/src/screens/ProfileScreen/ProfileScreen.js
@@ -5,93 +5,93 @@ import { auth, db } from '../../firebase/config'
 
 export default function ProfileScreen(props) {
 
-    // const [entityText, setEntityText] = useState('')
-    // const [entities, setEntities] = useState([])
+    const [messageText, setMessageText] = useState('')
+    const [messages, setMessages] = useState([])
 
-    // const entityRef = db.collection('entities')
+    const messageRef = db.collection('messages')
     const userID = props.extraData.id
     const userName = props.extraData.fullName
     const userEmail = props.extraData.email
 
-    // useEffect(() => {
-    //     entityRef
-    //         .where("authorID", "==", userID)
-    //         .orderBy('createdAt', 'desc')
-    //         .onSnapshot(
-    //             querySnapshot => {
-    //                 const newEntities = []
-    //                 querySnapshot.forEach(doc => {
-    //                     const entity = doc.data()
-    //                     entity.id = doc.id
-    //                     newEntities.push(entity)
-    //                 });
-    //                 setEntities(newEntities)
-    //             },
-    //             error => {
-    //                 console.log(error)
-    //             }
-    //         )
-    // }, [])
+    useEffect(() => {
+        messageRef
+            .where("authorID", "==", userID)
+            .orderBy('createdAt', 'desc')
+            .onSnapshot(
+                querySnapshot => {
+                    const newMessages = []
+                    querySnapshot.forEach(doc => {
+                        const message = doc.data()
+                        message.id = doc.id
+                        newMessages.push(message)
+                    });
+                    setMessages(newMessages)
+                },
+                error => {
+                    console.log(error)
+                }
+            )
+    }, [])
 
-    // const onAddButtonPress = () => {
-    //     if (entityText && entityText.length > 0) {
-    //         const timestamp = db.FieldValue.serverTimestamp();
-    //         const data = {
-    //             text: entityText,
-    //             authorID: userID,
-    //             createdAt: timestamp,
-    //         };
-    //         entityRef
-    //             .add(data)
-    //             .then(_doc => {
-    //                 setEntityText('')
-    //                 Keyboard.dismiss()
-    //             })
-    //             .catch((error) => {
-    //                 alert(error)
-    //             });
-    //     }
-    // }
+    const onAddButtonPress = () => {
+        if (messageText && messageText.length > 0) {
+            // const timestamp = db.FieldValue.serverTimestamp();
+            const data = {
+                text: messageText,
+                authorID: userID,
+                // createdAt: timestamp,
+            };
+            messageRef
+                .add(data)
+                .then(_doc => {
+                    setMessageText('')
+                    Keyboard.dismiss()
+                })
+                .catch((error) => {
+                    alert(error)
+                });
+        }
+    }
 
-    // const renderEntity = ({item, index}) => {
-    //     return (
-    //         <View style={styles.entityContainer}>
-    //             <Text style={styles.entityText}>
-    //                 {index}. {item.text}
-    //             </Text>
-    //         </View>
-    //     )
-    // }
+    const renderMessage = ({item, index}) => {
+        return (
+            <View style={styles.messageContainer}>
+                <Text style={styles.messageText}>
+                    {index}. {item.text}
+                </Text>
+            </View>
+        )
+    }
 
     return (
         <View style={styles.container}>
         <Text>{userID}</Text>
         <Text>{userName}</Text>
         <Text>{userEmail}</Text>
-            {/* <View style={styles.formContainer}>
+            <View style={styles.formContainer}>
                 <TextInput
                     style={styles.input}
-                    placeholder='Add new entity'
+                    placeholder='Send new message'
                     placeholderTextColor="#aaaaaa"
-                    onChangeText={(text) => setEntityText(text)}
-                    value={entityText}
+                    onChangeText={(text) => setMessageText(text)}
+                    value={messageText}
                     underlineColorAndroid="transparent"
                     autoCapitalize="none"
                 />
                 <TouchableOpacity style={styles.button} onPress={onAddButtonPress}>
-                    <Text style={styles.buttonText}>Add</Text>
+                    <Text style={styles.buttonText}>Send</Text>
                 </TouchableOpacity>
             </View>
-            { entities && (
+            { messages && (
                 <View style={styles.listContainer}>
                     <FlatList
-                        data={entities}
-                        renderItem={renderEntity}
+                        data={messages}
+                        renderItem={renderMessage}
                         keyExtractor={(item) => item.id}
                         removeClippedSubviews={true}
                     />
                 </View>
-            )} */}
+            )}
         </View>
     )
 }
